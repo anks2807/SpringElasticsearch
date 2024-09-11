5c0b8b4ac7eafc26f97fc1408c9cfb3f81e206e4
ankit.singhal <ankit.singhal@frequency.com>
2024-09-11 15:42:40 +0530

    code format

--- src/main/java/com/elasticsearch/demo/service/imple/EcommerceServiceImpl.java
+++ src/main/java/com/elasticsearch/demo/service/imple/EcommerceServiceImpl.java
@@ -31,19 +31,41 @@ import java.util.Map;
 @Service
 public class EcommerceServiceImpl implements EcommerceService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EcommerceServiceImpl.class);
     @Value("${ecommerce.index}")
     private String eCommerceIndex;
-
     @Autowired
     private RestHighLevelClient elasticsearchClient;
-
     @Autowired
     private ResourceLoader resourceLoader;
-
     @Autowired
     private ObjectMapper objectMapper;
 
-    private static final Logger logger = LoggerFactory.getLogger(EcommerceServiceImpl.class);
+    private static List<ECommerceData> loadData(File file) {
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy H:m");
+        List<ECommerceData> eCommerceDtos = null;
+        try (
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                CSVParser parser = CSVFormat.DEFAULT.withDelimiter(',').withHeader().parse(br);
+        ) {
+            eCommerceDtos = new ArrayList<>();
+            for (CSVRecord record : parser) {
+                ECommerceData eCommerceDto = new ECommerceData();
+                eCommerceDto.setCountry(record.get("Country"));
+                eCommerceDto.setInvoiceNo(record.get("InvoiceNo"));
+                eCommerceDto.setQuantity(record.get("Quantity") == null || record.get("Quantity").length() == 0 ? 0 : Long.parseLong(record.get("Quantity")));
+                eCommerceDto.setDescription(record.get("Description"));
+                eCommerceDto.setCustomerID(record.get("CustomerID") == null || record.get("CustomerID").length() == 0 ? 0 : Long.parseLong(record.get("CustomerID")));
+                eCommerceDto.setStockCode(record.get("StockCode"));
+                eCommerceDto.setUnitPrice(record.get("UnitPrice") == null || record.get("UnitPrice").length() == 0 ? 0 : Double.parseDouble(record.get("UnitPrice")));
+                eCommerceDto.setInvoiceDate(sdf.parse(record.get("InvoiceDate")));
+                eCommerceDtos.add(eCommerceDto);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return eCommerceDtos;
+    }
 
     @Override
     public ReturnMessageDto ingestBulkData() {
@@ -57,16 +79,16 @@ public class EcommerceServiceImpl implements EcommerceService {
         int startIndex = 0;
         int batchSize = 10000;
         int endIndex = batchSize;
-        while(endIndex <= eCommerceDtos.size()) {
-            for(int i = startIndex; i < endIndex; i++) {
+        while (endIndex <= eCommerceDtos.size()) {
+            for (int i = startIndex; i < endIndex; i++) {
                 IndexRequest indexRequest = new IndexRequest(eCommerceIndex).id(String.valueOf(i)).source(objectMapper.convertValue(eCommerceDtos.get(i), Map.class));
                 bulkRequest.add(indexRequest);
             }
 
 
-            try{
+            try {
                 elasticsearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
-            }catch (Exception exception) {
+            } catch (Exception exception) {
                 logger.error(exception.getMessage());
             }
             if (eCommerceDtos.size() - endIndex == 0) {
@@ -81,32 +103,6 @@ public class EcommerceServiceImpl implements EcommerceService {
         }
 
         return new ReturnMessageDto("Data ingested successfully", 200);
-    }
-
-    private static List<ECommerceData> loadData(File file) {
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy H:m");
-        List<ECommerceData> eCommerceDtos = null;
-        try (
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                CSVParser parser = CSVFormat.DEFAULT.withDelimiter(',').withHeader().parse(br);
-        ) {
-            eCommerceDtos = new ArrayList<>();
-            for (CSVRecord record : parser) {
-                ECommerceData eCommerceDto = new ECommerceData();
-                eCommerceDto.setCountry(record.get("Country"));
-                eCommerceDto.setInvoiceNo(record.get("InvoiceNo"));
-                eCommerceDto.setQuantity( record.get("Quantity") == null || record.get("Quantity").length() == 0 ? 0: Long.parseLong(record.get("Quantity")));
-                eCommerceDto.setDescription(record.get("Description"));
-                eCommerceDto.setCustomerID(record.get("CustomerID") == null || record.get("CustomerID").length() == 0 ? 0 : Long.parseLong(record.get("CustomerID")));
-                eCommerceDto.setStockCode(record.get("StockCode"));
-                eCommerceDto.setUnitPrice(record.get("UnitPrice") == null || record.get("UnitPrice").length() == 0 ? 0 : Double.parseDouble(record.get("UnitPrice")));
-                eCommerceDto.setInvoiceDate(sdf.parse(record.get("InvoiceDate")));
-                eCommerceDtos.add(eCommerceDto);
-            }
-        } catch (Exception e) {
-           logger.error(e.getMessage());
-        }
-        return eCommerceDtos;
     }
 
 }
